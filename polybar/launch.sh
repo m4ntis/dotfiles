806be7b52d3634320e74a3f8b65c018b70510155
Mati Shabtay <matishabtay@gmail.com>
2022-06-28 03:54:30 +0300

    xrandr: Configure multimon setup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+sleep 3
+xrandr --output eDP1 --left-of HDMI1 --output HDMI1 --mode 2560x1440
+~/.fehbg &
+
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar main &
+MONITOR=HDMI1 BARWIDTH=2510 polybar main &
 polybar dummy &
+MONITOR=HDMI1 polybar dummy &
 
 echo launched
